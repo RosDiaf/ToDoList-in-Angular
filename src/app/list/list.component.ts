dd19b0c7b15995aa027fb0097338c6aa72db8eb0
RosDiaf <ros_diaf@hotmail.com>
2019-06-25 23:48:32 +0100

    Sort items into localstorage

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -20,13 +20,22 @@ export class ListComponent implements OnInit, OnChanges {
     this.getTasks();
   }
 
+  setTasks() {
+    window.localStorage.setItem('items', JSON.stringify(this.taskListArr));
+  }
+
   getTasks() {
     this.taskListArr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
   }
 
+  sortTask() {
+    this.taskListArr.sort();
+    this.setTasks();
+  }
+
   deleteTask(index) {
     this.taskListArr.splice(index, 1);
-    window.localStorage.setItem('items', JSON.stringify(this.taskListArr));
+    this.setTasks();
   }
 
 }
