da2401e0e837797e0f87ef3c44d7cc5e95c11d84
RosDiaf <ros_diaf@hotmail.com>
2019-06-26 17:07:54 +0100

    Notify parent to delete task from list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges {
   title = 'Todolist';
-  status: boolean;
+  // status: boolean;
+  taskListArr = [];
+
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  ngOnChanges() {
+    this.getTasks();
+  }
+
+  setTasks() {
+    window.localStorage.setItem('items', JSON.stringify(this.taskListArr));
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.taskListArr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+  }
 
   showTasks() {
-    this.status = !this.status;
+    this.getTasks();
+  }
+
+  sortTask() {
+    this.taskListArr.sort();
+    this.setTasks();
+  }
+
+  deleteTask(index) {
+    this.taskListArr.splice(index, 1);
+    this.setTasks();
   }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,41 +1,27 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit, OnChanges {
+export class ListComponent implements OnInit {
 
-  @Input() status: boolean;
-  taskListArr = [];
+  @Input() taskListArr = [];
+  @Output() notifyParentToSortTask = new EventEmitter();
+  @Output() notifyParentToDeleteTask = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
-    this.getTasks();
-  }
-
-  ngOnChanges() {
-    this.getTasks();
-  }
-
-  setTasks() {
-    window.localStorage.setItem('items', JSON.stringify(this.taskListArr));
-  }
-
-  getTasks() {
-    this.taskListArr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
   }
 
   sortTask() {
-    this.taskListArr.sort();
-    this.setTasks();
+    this.notifyParentToSortTask.emit();
   }
 
   deleteTask(index) {
-    this.taskListArr.splice(index, 1);
-    this.setTasks();
+    this.notifyParentToDeleteTask.emit(index);
   }
 
 }
